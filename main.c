dc88b7200b5cc48fafd9e3ab809756dcc14d4f85
poisonlylo <tibichelyes@gmail.com>
2021-12-21 12:39:39 +0100

    creating a navbar

--- main.c
+++ main.c
@@ -3,11 +3,40 @@
 #include <stdlib.h>
 #include <string.h>
 #include <gtk/gtk.h>
+typedef struct{
+    char *label;
+    char sub_items;
+    char sub_menu[6][15];
+}MuButton;
 
-void close_window(){
-     gtk_main_quit();
+MuButton menulist[] = {
+          {"file", 6, {"new", "open","save", "save as", "close", "quit" }},
+         {"edit", 4,{"copy", "paste","cut", "delete"} }
+};
+void button_click(GtkWidget *button, gpointer data){
+    g_print("Click");
 
 }
+void close_window(){
+     gtk_main_quit();
+}
+void make_nemu(GtkWidget *vbox){
+    GtkWidget *menubar = gtk_menu_bar_new();
+    for (int i = 0; i < 7; ++i) {
+        GtkWidget *item = gtk_menu_item_new_with_label(menulist[i].label);
+        GtkWidget *item_menu = gtk_menu_new();
+        gtk_menu_shell_append(GTK_MENU_SHELL(menubar), item);
+        gtk_menu_item_set_submenu(GTK_MENU_ITEM(item), item_menu);
+        for (int j = 0; j < menulist->sub_items; ++j) {
+            GtkWidget *subitem = gtk_menu_item_new_with_label(menulist[i].sub_menu[j]);
+            g_signal_connect(GTK_WIDGET(subitem), "activate", G_CALLBACK(button_click),menulist[i].sub_menu[j] );
+            gtk_menu_shell_append(GTK_MENU_SHELL(item_menu), subitem);
+
+        }
+    }
+    gtk_container_add(GTK_CONTAINER(vbox), menubar);
+}
+
 void make_window(){
     GtkWidget *window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
     gtk_window_set_position(GTK_WINDOW(window), GTK_WIN_POS_CENTER);
@@ -17,6 +46,7 @@ void make_window(){
 
     GtkWidget *vbox = gtk_box_new(GTK_ORIENTATION_VERTICAL, 5);
     gtk_container_add(GTK_CONTAINER(window), vbox);
+    make_nemu(vbox);
     gtk_widget_show_all(window);
 }
 
@@ -25,6 +55,7 @@ void make_window(){
 
         gtk_init(&argc, &argv);
         make_window();
+
         gtk_main();
 
 /*
