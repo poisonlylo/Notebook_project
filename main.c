2903c05d6673d512c03fb519d082333c8dfe7a33
poisonlylo <tibichelyes@gmail.com>
2021-12-21 11:18:43 +0100

    creating a simple window with a vbox

--- main.c
+++ main.c
@@ -4,8 +4,30 @@
 #include <string.h>
 #include <gtk/gtk.h>
 
+void close_window(){
+     gtk_main_quit();
+
+}
+void make_window(){
+    GtkWidget *window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+    gtk_window_set_position(GTK_WINDOW(window), GTK_WIN_POS_CENTER);
+    gtk_window_set_default_size(GTK_WINDOW(window), 400, 400);
+    gtk_window_set_title(GTK_WINDOW(window), "Login");
+    g_signal_connect(G_OBJECT(window), "destroy", G_CALLBACK(close_window), NULL);
+
+    GtkWidget *vbox = gtk_box_new(GTK_ORIENTATION_VERTICAL, 5);
+    gtk_container_add(GTK_CONTAINER(window), vbox);
+    gtk_widget_show_all(window);
+}
+
+
     int main(int argc, char **argv){
 
+        gtk_init(&argc, &argv);
+        make_window();
+        gtk_main();
+
+/*
         char username[255];
         char password[255];
         char user[15];
@@ -13,6 +35,8 @@
         char email[255];
         int id;
         char requete[255];
+
+
 
 
 
@@ -44,7 +68,8 @@
             printf("Une erreur s'est produite lors de la connexion à la BDD!");
         }
 
-
+*/
 return 0;
+
     }
 
